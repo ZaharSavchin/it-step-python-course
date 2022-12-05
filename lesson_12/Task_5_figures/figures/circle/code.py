037d1dd480f60de6622c42b44db70ed24189cb5d
ZaharSavchin <pchelovod.pasechnik@mail.ru>
2022-12-05 14:27:07 +0300

    lesson_12

---
+++ lesson_12/Task_5_figures/figures/circle/code.py
@@ -0,0 +1,6 @@
+def circle_area(default_radius=5, pi=3.14):
+    return pi * default_radius**2
+
+
+def circle_perimeter(default_radius=5, pi=3.14):
+    return 2*pi*default_radius
---
+++ lesson_12/Task_5_figures/main_tese.py
@@ -0,0 +1,9 @@
+from figures import triangle_area, triangle_perimeter, square_perimeter, square_area, circle_area, circle_perimeter
+
+
+print(triangle_area())
+print(triangle_perimeter())
+print(square_perimeter())
+print(square_area())
+print(circle_area())
+print(circle_perimeter())
